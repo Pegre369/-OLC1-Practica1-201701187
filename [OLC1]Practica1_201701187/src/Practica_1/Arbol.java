413371487b2dfab980302dd36b3f9f28dc90f62e
Pedro <pedro.ordoniez@gmail.com>
2020-02-15 01:40:12 -0600

    Arbol armado, punteros malos

--- [OLC1]Practica1_201701187/src/Practica_1/Arbol.java
+++ [OLC1]Practica1_201701187/src/Practica_1/Arbol.java
@@ -2,95 +2,115 @@ package Practica_1;
 
 import java.util.ArrayList;
 
-
 public class Arbol {
-    
+
     //Clase del nodo
-     class Nodo {
-        
+    class Nodo {
+
         public String etiquetas;
         public Nodo derecha;
         public Nodo izquierda;
         public String descripcion;
-       
-        public Nodo(String etiqueta, String des){
-            
+
+        public Nodo(String etiqueta, String des) {
+
             this.etiquetas = etiqueta;
             this.descripcion = des;
             this.derecha = null;
             this.izquierda = null;
-            
+
         }
-        
-       }
-     
-     
-    // Clase del arbol
-     
-     Nodo raiz;
-     public static ArrayList<Lista_ER> cara;
-     
-       
-    public Arbol() {
-        
-        raiz = null;
-        
+
     }
-     
-     public void Asignacion(ArrayList<Lista_ER> Caracteres){
-        
+
+    // Clase del arbol
+    Nodo raiz;
+    public static ArrayList<Lista_ER> cara;
+
+    public Arbol() {
+
+        raiz = null;
+
+    }
+
+    public void Asignacion(ArrayList<Lista_ER> Caracteres) {
+
         cara = Caracteres;
 
-        
-        for(int i = 0; i<cara.size();i++){
-            
-           // System.out.println(cara.get(i).getEtiqueta()+ " -> "+cara.get(i).getDescripcion());
-            
+        for (int i = 0; i < cara.size(); i++) {
+
+            //System.out.println(cara.get(i).getEtiqueta() + " -> " + cara.get(i).getDescripcion());
+            agregar(cara.get(i).getEtiqueta(), cara.get(i).getDescripcion());
+
         }
-         
-        
-     }
-   
-     public void agregarnodo(String etiqueta, String desc){
-         
-         Nodo nuevo = new Nodo(etiqueta, desc);
-         
-         if(raiz==null){
-              raiz=nuevo;
-         
-         }else if(raiz.izquierda==null){
+
+        recoridopreorder(raiz);
+
+    }
+
+    public Nodo agregarnodo(Nodo raiz, String etiqueta, String desc) {
+
+        if (raiz == null) {
+
+            return new Nodo(etiqueta, desc);
+
+        }
+
+        if (desc == "Concatenacion" || desc == "Or") {
+
+            if (raiz.izquierda == null && raiz.derecha == null) {
+
+                raiz.izquierda = agregarnodo(raiz.izquierda, etiqueta, desc);
+
+            } else if (raiz.izquierda != null && raiz.derecha == null) {
+
+                raiz.derecha = agregarnodo(raiz.izquierda, etiqueta, desc);
+
+            }else{
+                return raiz;
+            }
+
+        } else if (desc == "kleen" || desc == "positiva" || desc == "aparicion") {
+
+            if (raiz.izquierda != null && raiz.derecha == null) {
+                raiz.derecha = agregarnodo(raiz.derecha, etiqueta, desc);
+            } else {
+                raiz.izquierda = agregarnodo(raiz.izquierda, etiqueta, desc);
+            }
             
-             
-             
-         }else if(raiz.derecha==null){
-             
-             
-         }
-             
-         
-     }
-     
+            if(raiz.izquierda.descripcion == "kleen"){
+                
+            }
+            
+            
+
+        } else if (desc == "identificador" || desc == "cadena") {
+            if (raiz.izquierda == null && raiz.derecha == null) {
+
+                raiz.izquierda = agregarnodo(raiz.izquierda, etiqueta, desc);
+
+            } else if (raiz.izquierda != null && raiz.derecha == null) {
+
+                raiz.derecha = agregarnodo(raiz.izquierda, etiqueta, desc);
+
+            } else {
+                return raiz;
+            }
+        }
+
+        return raiz;
+    }
+
+    public void agregar(String etiqueta, String desc) {
+        raiz = agregarnodo(raiz, etiqueta, desc);
+    }
+
+    public void recoridopreorder(Nodo nodo) {
+        if (nodo != null) {
+            recoridopreorder(nodo.izquierda);
+            recoridopreorder(nodo.derecha);
+            System.out.print(" " + nodo.etiquetas);
+        }
+    }
+
 }
-
-
-
-
-/*
-Nodo auxiliar = raiz;
-             Nodo padre;
-             
-             while (true) {
-                   
-                 padre = auxiliar;
-                 if(desc=="Concatenacion"){
-                     auxiliar=auxiliar.izquierda;
-                     if(auxiliar==null){
-                         padre.izquierda = nuevo;
-                         return;
-                     }
-                 }
-                 
-                 
-             }
-             
-*/
